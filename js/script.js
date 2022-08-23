955541862f152dace5ccf9087fd2aacb8cdb4627
julia246135 <jujuheidrich@gmail.com>
2022-08-23 08:43:28 -0300

    main

--- js/script.js
+++ js/script.js
@@ -18,4 +18,30 @@ $(document).ready(function() {
         $('#mensagem').empty()
     })
 
+    //Botão de resumo
+
+    $('p').hide()
+
+    $('#btn-resumo').click(function(e) {
+        e.preventDefault()
+
+        //recebe o status
+        let status = $(this).attr('data-status')
+
+        $('#btn-resumo').empty()
+        if (status == '1') {
+
+            $('#btn-resumo').append('Esconder resumo')
+            $('#btn-resumo').attr('data-status', '2')
+        } else {
+
+            $('#btn-resumo').append('Ver resumo')
+            $('#btn-resumo').attr('data-status', '1')
+        }
+
+        $('p').toggle(2000)
+    })
+
+    //2000 -> 2 segundos
+
 })
